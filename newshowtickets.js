dc445e90ff6b91dbe015278f99e05e81f98d33dc
amolc <amol.chawathe@fountaintechies.com>
2017-11-17 11:46:40 +0800

    New Server

--- newshowtickets.js
+++ newshowtickets.js
@@ -37,7 +37,7 @@ app.post('/api/addbankorder', order.addbankorder);
 app.get('/api/get-data', order.getdata);
 app.get('/api/get-product-data/:id', order.getproductdata);
 
-app.listen(9006, function () {
-  console.log('CORS-enabled web server listening on port 9006')
+app.listen(6006, function () {
+  console.log('CORS-enabled web server listening on port 6006')
 })
-console.log("Magic at 9006");
+console.log("Magic at 6006");
